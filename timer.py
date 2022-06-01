f9c9633f5a98aed885e112c38359bab6444057b4
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-06-01 08:56:05 -0500

    Calculates days too

--- timer.py
+++ timer.py
@@ -4,13 +4,14 @@ timers_seconds = int(input("Seconds for your timer: "))
 reminder = "Go for a run now!"
 
 while timers_seconds > 0:
-    hours = int(timers_seconds/3600)
+    days = int(timers_seconds/86400)
+    hours = int( (timers_seconds % 86400) / 3600 )
     # calculate the number of minutes remaining after we remove the hours portion.
     minutes = int( (timers_seconds % 3600) / 60)
     # calculates the number of seconds remaining
     seconds = int( (timers_seconds % 3600) % 60 )
 
-    print(f"{hours}:{minutes}:{seconds}")
+    print(f"{days}:{hours}:{minutes}:{seconds}")
 
     tm.sleep(1)
     
