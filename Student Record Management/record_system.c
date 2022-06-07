31c4f96f85797184359bfdfc96eef407cd66abf2
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-06-07 00:19:39 -0500

    Creates a for loop that asks for 5 students.

--- Student Record Management/record_system.c
+++ Student Record Management/record_system.c
@@ -1,34 +1,43 @@
 #include "stdio.h"
 #include <stdlib.h>
 
-void writeRecords(char*);
+void writeRecords(char*, char[], int, float);
 
 int main(void)
 {
-    char name[15];
-    int age;
-    float gpa;
+    // Create 5 student records
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Enter information for record #%i \n", i+1);
 
-    printf("Please enter your name: ");
-    scanf("%s", name);
+        char name[15];
+        int age;
+        float gpa;
 
-    printf("How old are you? ");
-    scanf("%i", &age);
+        printf("Please enter your name: ");
+        scanf("%s", name);
 
-    printf("Ok, finally %s, what's your current GPA? ", name);
-    scanf("%f", &gpa);
+        printf("How old are you? ");
+        scanf("%i", &age);
 
-    printf("Okay. So, your name is %s, you're %i, and your GPA is %0.1f.\n", name, age, gpa);
+        printf("Ok, finally %s, what's your current GPA? ", name);
+        scanf("%f", &gpa);
+
+        printf("Okay. So, your name is %s, you're %i, and your GPA is %0.1f.\n", name, age, gpa);
+
+        writeRecords("student_record.txt", name, age, gpa);   
+    }
 
     return 0;
 }
 
 // Write records to a designated filename
-void writeRecords(char* filename)
+void writeRecords(char* filename, char name[15], int age, float gpa)
 {
-    FILE *filePointer = fopen(filename, "w");
+    // Opens the desired file on "append" mode.
+    FILE *filePointer = fopen(filename, "a");
 
-    fprintf(filePointer, "%s %0.2f", "We are mighty!", 2.0);
+    fprintf(filePointer, "%s %i %0.2f\n", name, age, gpa);
 
     // fclose calls the free() function and closes the file
     fclose(filePointer);
