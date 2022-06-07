29bf5d4bf847ba0f5e2c1196ddaa1e8b14e74ea3
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-06-07 00:08:43 -0500

    Fixed formatting. Fixed string creation.

--- Student Record Management/record_system.c
+++ Student Record Management/record_system.c
@@ -1,10 +1,24 @@
 #include "stdio.h"
+#include <stdlib.h>
 
 void writeRecords(char*);
 
 int main(void)
 {
-    writeRecords("record_system.txt");
+    char name[15];
+    int age;
+    float gpa;
+
+    printf("Please enter your name: ");
+    scanf("%s", name);
+
+    printf("How old are you? ");
+    scanf("%i", &age);
+
+    printf("Ok, finally %s, what's your current GPA? ", name);
+    scanf("%f", &gpa);
+
+    printf("Okay. So, your name is %s, you're %i, and your GPA is %0.1f.\n", name, age, gpa);
 
     return 0;
 }
