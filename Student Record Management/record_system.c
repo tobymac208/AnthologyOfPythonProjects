958119e8e35b21ca856fb6aa18d525d6f561242b
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-06-07 00:35:54 -0500

    working on i

--- Student Record Management/record_system.c
+++ Student Record Management/record_system.c
@@ -1,43 +1,46 @@
 #include "stdio.h"
 #include <stdlib.h>
 
-void writeRecords(char*, char[], int, float);
+void writeRecords(char*, char[], int, char[]);
 
 int main(void)
 {
+    // Defines how many records the loop should ask for.
+    const int REQUEST_RECORDS = 2;
+
     // Create 5 student records
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < REQUEST_RECORDS; i++)
     {
         printf("Enter information for record #%i \n", i+1);
 
         char name[15];
-        int age;
-        float gpa;
+        int rollNumber;
+        char subject[25];
 
         printf("Please enter your name: ");
         scanf("%s", name);
 
-        printf("How old are you? ");
-        scanf("%i", &age);
+        printf("Roll number: ");
+        scanf("%i", &rollNumber);
 
-        printf("Ok, finally %s, what's your current GPA? ", name);
-        scanf("%f", &gpa);
+        printf("Subject: ");
+        scanf("%s", subject);
 
-        printf("Okay. So, your name is %s, you're %i, and your GPA is %0.1f.\n", name, age, gpa);
+        printf("Okay. So, your name is %s, you roll number is %i, and your subject is %s.\n", name, rollNumber, subject);
 
-        writeRecords("student_record.txt", name, age, gpa);   
+        writeRecords("student_record.txt", name, rollNumber, subject);   
     }
 
     return 0;
 }
 
 // Write records to a designated filename
-void writeRecords(char* filename, char name[15], int age, float gpa)
+void writeRecords(char* filename, char name[15], int age, char subject[])
 {
     // Opens the desired file on "append" mode.
     FILE *filePointer = fopen(filename, "a");
 
-    fprintf(filePointer, "%s %i %0.2f\n", name, age, gpa);
+    fprintf(filePointer, "%s %i %s\n", name, age, subject);
 
     // fclose calls the free() function and closes the file
     fclose(filePointer);
